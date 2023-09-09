b8b18cef883194536365d54808861a08050930e0
Kjeez <86542567+Kjeez@users.noreply.github.com>
2023-09-09 19:01:12 +0530

    circle fixed again

--- components/Circles.js
+++ components/Circles.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-8 bottom-5 xl:-right-16 xl:-bottom-2 mix-blend-color-dodge
+    <div className="w-[200px] xl:w-[300px] absolute -right-10 bottom-10 xl:-right-16 xl:-bottom-2 mix-blend-color-dodge
     animate-pulse duration-75 z-10">
       <Image src={'/circles.png'} width={260} height={200}
       className="w-full h-full"
